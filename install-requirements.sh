d6f1cd0031a007bd4d9405fd61c1d9c8b91a9e99
SoonminHwang <jjang9hsm@gmail.com>
2016-09-04 22:19:47 +0200

    Remove chrischoy's settings

--- install-requirements.sh
+++ install-requirements.sh
@@ -5,34 +5,15 @@ sudo apt-get install -y python-software-properties software-properties-common
 sudo apt-get install exuberant-ctags zsh tig htop
 sudo apt-get install dconf-tools
 
-# Python
-# User should choose which python packages to install through pip vs. apt-get
-# sudo apt-get install python-dev python-pip python3-dev python3-pip
-# sudo apt-get install python-pip python3-pip
-# sudo apt-get install ipython
-# sudo apt-get install python-ipdb
-
-# For screen highlight
-# http://askubuntu.com/questions/665155/how-to-highlight-current-screen-or-window
-sudo apt-get install wmctrl
-
-# Add PPAs
-sudo add-apt-repository ppa:webupd8team/y-ppa-manager  # ppa-manager
-sudo add-apt-repository ppa:neovim-ppa/unstable  # Neovim
-sudo add-apt-repository ppa:byobu/ppa  # tmux
-sudo apt-add-repository ppa:izx/askubuntu -y  # CompizConfig manager
-sudo apt-get update
-
-# Install neovim, tmux, GUI ppa-manager
-sudo apt-get install y-ppa-manager
-sudo apt-get install neovim
-sudo apt-get install tmux=2.1-3build1~trusty
-sudo apt-get install unit  # CompizConfig manager
-
-# Install python support for neovim
-# See https://neovim.io/doc/user/provider.html#provider-python
-sudo pip install neovim
-sudo pip3 install neovim  # for python3 plugins
+# Python - anaconda
+wget https://repo.continuum.io/archive/Anaconda2-4.1.1-Linux-x86_64.sh -O /tmp
+bash /tmp/Anaconda3-4.0.0-Linux-x86_64.sh
+echo 'export PATH=/home/soonminh/anaconda2/bin:$PATH' >> ~/.bashrc 
+### User should choose which python packages to install through pip vs. apt-get
+### sudo apt-get install python-dev python-pip python3-dev python3-pip
+### sudo apt-get install python-pip python3-pip
+### sudo apt-get install ipython
+### sudo apt-get install python-ipdb
 
 # Install python packages
 sudo pip install grip  # Markdown renderer
--- setup-dev-environment.sh
+++ setup-dev-environment.sh
@@ -1,37 +1,7 @@
 # Generate a ssh key.
-ssh-keygen -t rsa -b 4096 -C "chrischoy@ai.stanford.edu"
+ssh-keygen -t rsa -b 4096 -C "jjang9hsm@gmail.com"
 
-# Neovim vim-plug install
-curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-# TPM
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-# Link vimrc
-ln config/.nvimrc ~/.config/nvim/init.vim
-ln config/.nvimrc ~/.nvimrc  # simpler path to find the file quickly
-ln config/.tmux.conf ~/.tmux.conf  
-ln config/.gitconfig ~/.gitconfig
 
 # Set up my profile
-git config --global user.email $1
-git config --global user.name $2
-
-# Oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-# Download my custom theme
-mkdir $ZSH_CUSTOM/themes
-curl -fsSL -o $ZSH_CUSTOM/themes/capri7.zsh-theme https://gist.githubusercontent.com/chrischoy/a1a4f3e9468efd61a4ff4e3f67c945c5/raw/capri7.zsh-theme
-
-# Powerline fonts
-# Once installed, manually set the terminal font to be one of the powerline fonts
-git clone https://github.com/powerline/fonts
-sh fonts/install.sh
-
-# Follow the steps in the URL[1] to remove the show desktop icon
-# [1] http://askubuntu.com/questions/167263/how-can-i-remove-show-desktop-from-the-alt-tab-application-switcher
-
-# Diable mouse acceleration
-xset m 0 0 &
+git config --global user.email 'jjang9hsm@gmail.com'
+git config --global user.name 'Soonmin Hwang'
